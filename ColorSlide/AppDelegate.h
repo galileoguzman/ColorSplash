39846f15b23e22fec0d055b0498a8239ffb94802
Lamb11 <abc_1118@hotmail.com>
2015-01-26 15:44:18 -0600

    Initial Commit

---
+++ ColorSlide/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ColorSlide
+//
+//  Created by Alberto Cordero Arellanes on 26/01/15.
+//  Copyright (c) 2015 AlbertoCorderoArellanes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ColorSlide/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ColorSlide
+//
+//  Created by Alberto Cordero Arellanes on 26/01/15.
+//  Copyright (c) 2015 AlbertoCorderoArellanes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
